54b846b116abbd1fc38a3152f93dc35fce2ba55b
saahil1206 <saahil1206@outlook.com>
2026-04-15 16:12:21 +0530

    xyz

--- madhurbazarbackend/src/routes/user.routes.js
+++ madhurbazarbackend/src/routes/user.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { profileController } = require("../controllers/user.controller");
-const authMiddleware = require("../middlewares/auth.middleware");
+// const authMiddleware = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
-router.get("/profile", authMiddleware, profileController);
-
+// router.get("/profile", authMiddleware, profileController);
+router.get("/profile", profileController);
 module.exports = router;
 
 
