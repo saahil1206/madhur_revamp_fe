0fee58ee00e8a51136a5abfb59ee7caa36158676
saahil1206 <saahil1206@outlook.com>
2026-04-15 15:53:14 +0530

    console_profile_3

--- madhurbazarbackend/src/routes/user.routes.js
+++ madhurbazarbackend/src/routes/user.routes.js
@@ -1,25 +1,25 @@
-// const express = require("express");
-// const { profileController } = require("../controllers/user.controller");
-// const authMiddleware = require("../middlewares/auth.middleware");
-
-// const router = express.Router();
-
-// router.get("/profile", authMiddleware, profileController);
-
-// module.exports = router;
-
-
-
 const express = require("express");
 const { profileController } = require("../controllers/user.controller");
-const { updateProfileController } = require("../controllers/auth.controller"); // 👈 add this
 const authMiddleware = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
 router.get("/profile", authMiddleware, profileController);
 
-// ✅ ADD THIS
-router.put("/profile", authMiddleware, updateProfileController);
-
 module.exports = router;
+
+
+
+// const express = require("express");
+// const { profileController } = require("../controllers/user.controller");
+// const { updateProfileController } = require("../controllers/auth.controller"); // 👈 add this
+// const authMiddleware = require("../middlewares/auth.middleware");
+
+// const router = express.Router();
+
+// router.get("/profile", authMiddleware, profileController);
+
+// // ✅ ADD THIS
+// // router.put("/profile", authMiddleware, updateProfileController);
+
+// module.exports = router;
